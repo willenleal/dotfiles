7b219be16420b59d05078c840084b9a1fc4f10a7
Willen Leal <willenlealdev@gmail.com>
2024-08-12 00:39:12 -0400

    customized greeting screen

---
+++ nvim/lua/plugins/alpha.lua
@@ -0,0 +1,40 @@
+return {
+	"goolord/alpha-nvim",
+	dependencies = { "echasnovski/mini.icons" },
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
+
+		-- Footer
+		local function footer()
+			return ""
+		end
+
+		-- Banner
+		local banner = {
+			[[                                                    ]],
+			[[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ]],
+			[[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ]],
+			[[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ]],
+			[[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ]],
+			[[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ]],
+			[[ ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ]],
+			[[                                                    ]],
+		}
+
+		dashboard.section.header.val = banner
+
+		-- Menu
+		dashboard.section.buttons.val = {
+			dashboard.button("e", "  New file", ":ene <BAR> startinsert<CR>"),
+			dashboard.button("r", "󰈢  Recently opened files", ":Telescope oldfiles<CR>"),
+			dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
+			dashboard.button("u", "  Update plugins", ":Lazy update<CR>"),
+			dashboard.button("q", "  Quit", ":qa<CR>"),
+		}
+
+		dashboard.section.footer.val = footer()
+
+		alpha.setup(dashboard.config)
+	end,
+}
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,19 +1,27 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    version = '0.1.4',
-    dependencies = {{'nvim-lua/plenary.nvim'}},
-    config = function()
-        local telescope = require("telescope")
-        local builtin = require('telescope.builtin')
+	"nvim-telescope/telescope.nvim",
+	version = "0.1.4",
+	dependencies = { { "nvim-lua/plenary.nvim" } },
+	config = function()
+		local telescope = require("telescope")
+		local builtin = require("telescope.builtin")
 
-        telescope.load_extension('noice')
+		telescope.setup({
+			pickers = {
+				oldfiles = {
+					cwd_only = true,
+				},
+			},
+		})
 
-        vim.keymap.set('n', '<leader>pf', function()
-            builtin.find_files({no_ignore = false, hidden = true})
-        end, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.live_grep({additional_args = {"--hidden"}})
-        end, {})
-    end
+		telescope.load_extension("noice")
+
+		vim.keymap.set("n", "<leader>pf", function()
+			builtin.find_files({ no_ignore = false, hidden = true })
+		end, {})
+		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+		vim.keymap.set("n", "<leader>ps", function()
+			builtin.live_grep({ additional_args = { "--hidden" } })
+		end, {})
+	end,
 }
